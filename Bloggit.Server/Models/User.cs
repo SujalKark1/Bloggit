9044c55bedbfc72af837826c3755bd121248861d
anisha0729 <140361152+anisha0729@users.noreply.github.com>
2024-05-03 11:01:59 +0545

    Initial Commit

---
+++ Bloggit.Server/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bloggit.Server.Models
+{
+    public class User : IdentityUser
+    {
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "timestamp with time zone")]
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        [Column(TypeName = "timestamp with time zone")]
+        public DateTime ModifiedDate { get; set; } = DateTime.UtcNow;
+
+        [Column(TypeName = "timestamp with time zone")]
+        public DateTime LastLogin { get; set; } = DateTime.UtcNow;
+
+        public bool IsAdmin { get; set; } = false;
+    }
+}
